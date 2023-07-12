039d887d19b263e603aae884d12948b06000a610
ju-lit-hein <julien.ferdinand@epitech.eu>
2023-07-12 21:58:42 +0400

    fix: Fix merge problem

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 git switch react-release
-git pull --force origin react
+git reset --hard react
 npm run build
 find . -maxdepth 1 -name "*" ! -name "build" -delete
 mv build/* .
 rmdir build
 git add --all
 git commit -m "build: Build React at $(date -u "+%h-%d-%Y %H:%M:%S")"
-git push origin react-release
+git push --force origin
 git switch react
